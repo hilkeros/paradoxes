d3e9744638e3dc07795e802ca00c4a69400d5076
Hilke Ros <hilkeros@gmail.com>
2021-03-12 16:57:03 +0100

    eternal image loop scroll

--- v1/sketch.js
+++ v1/sketch.js
@@ -1,6 +1,7 @@
 // a shader variable
 let theShader;
 let images;
+let canvasSize;
 
 function preload(){
   // load the shader
@@ -15,19 +16,21 @@ function preload(){
 function setup() {
   // shaders require WEBGL mode to work
   // createCanvas(windowWidth, windowHeight, WEBGL);
-  let canvas = createCanvas(400, 400, WEBGL);
+
+  canvasSize = 600;
+  let canvas = createCanvas(canvasSize, canvasSize, WEBGL);
   canvas.parent('canvas-holder');
   noStroke();
 }
 
 function draw() {
   let scrollY = window.pageYOffset;
-  let y = scrollY % 800;
-  y = map(y, 0, 800, 800, 0);
-  let activeImg = Math.floor(scrollY/800);
-  if (activeImg > 2) { activeImg = 2};
+  let y = scrollY % (canvasSize * 2);
+  y = map(y, 0, canvasSize * 2, canvasSize * 2, 0);
+  let activeWindowPart = Math.floor(scrollY/(canvasSize * 2));
+  let activeImage = activeWindowPart % images.length;
 
-  // console.log(activeImg);
+  // console.log(activeImg, activeReset);
   
   // shader() sets the active shader with our shader
   shader(theShader);
@@ -35,9 +38,14 @@ function draw() {
   // theShader.setUniform("u_time", millis() / 1000.0); // we divide millis by 1000 to convert it to seconds
   // theShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]); // we flip Y so it's oriented properly in our shader
 
-  theShader.setUniform('u_tex0', images[activeImg]);
-  if (activeImg < 2) {
-    theShader.setUniform('u_tex1', images[activeImg + 1]);
+  theShader.setUniform('u_tex0', images[activeImage]);
+
+  // take the first image again if we reached the end of the image list
+  if (activeImage === (images.length - 1)) {
+    theShader.setUniform('u_tex1', images[0]);
+  // else take the next image
+  } else {
+    theShader.setUniform('u_tex1', images[activeImage+1]);
   }
 
   theShader.setUniform('scroll_y', y);
