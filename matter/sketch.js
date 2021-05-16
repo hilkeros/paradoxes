5d19b0021307ae7976dbd1bda41d859bd5cda5fa
Hilke Ros <hilkeros@gmail.com>
2021-05-16 18:24:14 +0200

    improve hearts

--- matter/sketch.js
+++ matter/sketch.js
@@ -1,19 +1,10 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/urR596FsU68
-
+const d = "M484,443s92.75-58.07,110.09-116.83C603,296,598,243,544,243c-49.55,0-58.61,42.56-60,51.93-1.39-9.37-10.45-51.93-60-51.93-54,0-59,53-50.09,83.17C391.25,384.93,484,443,484,443Z";
 var canvas;
-// module aliases
-var Engine = Matter.Engine,
-  // Render = Matter.Render,
-  World = Matter.World,
-  Bodies = Matter.Bodies;
+const { Engine, World, Bodies } = Matter;
 
 var engine;
 var world;
-var boxes = [];
-
+var hearts = [];
 var ground;
 var heartImage;
 
@@ -24,14 +15,13 @@ function preload() {
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('canvas-holder');
-  // rectMode(CENTER);
+  rectMode(CENTER);
   engine = Engine.create();
   world = engine.world;
-  //Engine.run(engine);
   var options = {
     isStatic: true
   };
-  ground = Bodies.rectangle(0, height, width * 2, 50, options);
+  ground = Bodies.rectangle(width/2, height-10, width, 20, options);
 
   World.add(world, ground);
 }
@@ -46,25 +36,25 @@ function touchStarted() {
 
 function showHearts() {
   for (var i = 0; i < 6; i++) {
-    boxes.push(new Heart(random(width / 5) + width / 2, random(height / 5), random(20, 180)));
+    hearts.push(new Heart(random(width / 2) + width / 4, random(height / 5), random(20, 80)));
   }
 }
 
 function draw() {
   clear();
-  Engine.update(engine);
-  boxes.map(box => box.show());
-  noStroke(255);
+  Engine.update(engine, 1000/120);
+  hearts.map(heart => heart.show());
+  noStroke();
   fill(170);
-  rect(0, height - 25, width, 25);
+  rect(width/2, height-5, width, 10);
 }
 
 function Heart(x, y, w) {
   var options = {
-    friction: 0.1,
+    friction: 0.8,
     restitution: 0.1
   };
-  this.body = Bodies.rectangle(x, y, w, w, options);
+  this.body = Bodies.polygon(x, y, 3, w, options);
   this.w = w;
   World.add(world, this.body);
 
@@ -74,7 +64,22 @@ function Heart(x, y, w) {
     push();
     translate(pos.x, pos.y);
     rotate(angle);
-    image(heartImage, 0, 0, this.w, this.w);
+    rectMode(CENTER);
+    stroke(255);
+    // polygon(0, 0, this.w, 3);
+    imageMode(CENTER);
+    image(heartImage, 0, 0, this.w * 2, this.w * 2);
     pop();
   };
+}
+
+function polygon(x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
