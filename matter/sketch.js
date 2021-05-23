60deeb0c85e21e5fb6aebea8282775e37db07975
Hilke Ros <hilkeros@gmail.com>
2021-05-23 16:37:51 +0200

    added heart vertices

--- matter/sketch.js
+++ matter/sketch.js
@@ -1,12 +1,13 @@
-const d = "M484,443s92.75-58.07,110.09-116.83C603,296,598,243,544,243c-49.55,0-58.61,42.56-60,51.93-1.39-9.37-10.45-51.93-60-51.93-54,0-59,53-50.09,83.17C391.25,384.93,484,443,484,443Z";
+var path;
 var canvas;
-const { Engine, World, Bodies } = Matter;
+const { Engine, World, Bodies, Svg, Vertices } = Matter;
 
 var engine;
 var world;
 var hearts = [];
 var ground;
 var heartImage;
+var heartVertices;
 
 function preload() {
   heartImage = loadImage('../greta/heart.png');
@@ -24,10 +25,9 @@ function setup() {
   ground = Bodies.rectangle(width/2, height-10, width, 20, options);
 
   World.add(world, ground);
-}
-
-function mouseClicked() {
-  showHearts();
+  path = document.getElementById('heart-path');
+  heartVertices = Svg.pathToVertices(path);
+  heartVertices = decomp.quickDecomp(heartVertices);
 }
 
 function touchStarted() {
@@ -36,26 +36,29 @@ function touchStarted() {
 
 function showHearts() {
   for (var i = 0; i < 6; i++) {
-    hearts.push(new Heart(random(width / 2) + width / 4, random(height / 5), random(20, 80)));
+    hearts.push(new Heart(random(width / 2) + width / 4, random(height / 5), 200));
   }
 }
 
 function draw() {
   clear();
-  Engine.update(engine, 1000/120);
+  Engine.update(engine, 1000/70);
   hearts.map(heart => heart.show());
   noStroke();
   fill(170);
-  rect(width/2, height-5, width, 10);
+  rect(width/2, height-20, width, 40);
 }
 
 function Heart(x, y, w) {
   var options = {
-    friction: 0.8,
-    restitution: 0.1
+    friction: 0.1,
+    restitution: 0.8
   };
-  this.body = Bodies.polygon(x, y, 3, w, options);
+
   this.w = w;
+  let scaledVertices = Vertices.scale(heartVertices, this.w/200, this.w/200);
+  this.body = Bodies.fromVertices(x, y, scaledVertices);
+ 
   World.add(world, this.body);
 
   this.show = function () {
@@ -66,20 +69,8 @@ function Heart(x, y, w) {
     rotate(angle);
     rectMode(CENTER);
     stroke(255);
-    // polygon(0, 0, this.w, 3);
     imageMode(CENTER);
-    image(heartImage, 0, 0, this.w * 2, this.w * 2);
+    image(heartImage, 0, 0, this.w, this.w);
     pop();
   };
-}
-
-function polygon(x, y, radius, npoints) {
-  let angle = TWO_PI / npoints;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius;
-    let sy = y + sin(a) * radius;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
 }
